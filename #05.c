86e179efa3b5ef1b07625ad7d0064174413a0cff
pedromaia959 <92109572+pedromaia959@users.noreply.github.com>
2021-12-14 16:48:16 -0300

    Add files via upload

---
+++ #05.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+  int i;
+  float notas[4999]
+  
+  printf("Valores do Vetor \n");
+  
+  for( i = 0 ; i <= 4; i++)
+  {
+    printf("notas[%d] = %.1f\n",i, notas[i]);
+  }
+
+return 0;
+}
